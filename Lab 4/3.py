720dd72c40b53d275615ee5f60faa71734294cb2
Виктория Романова <147710292+soulvi@users.noreply.github.com>
2023-12-24 16:50:05 +0500

    Update 3.py

--- Lab 4/3.py
+++ Lab 4/3.py
@@ -4,58 +4,24 @@ import json
 import sqlite3
 
 def decode_msgpack(file_name1):
-    items1 = []
-    with open(file_name1, "rb") as file:
-        decoded_data = file.read()
-        data = msgpack.unpackb(decoded_data)
-        for row in data[1:]:
-            artist = row['artist']
-            song = row['song']
-            duration_ms = row['duration_ms']
-            year = row['year']
-            tempo = row['tempo']
-            genre = row['genre']
-            mode = row['mode']
-            speechiness = row['speechiness']
-            acousticness = row['acousticness']
-            instrumentalness = row['instrumentalness']
+    with open(file_name1, 'rb') as f:
+        songs_msgpack = msgpack.load(f)
 
-            songs = {
-                'artist': artist,
-                'song': song,
-                'duration_ms': duration_ms,
-                'year': year,
-                'tempo': tempo,
-                'genre': genre,
-                'mode': mode,
-                'speechiness': speechiness,
-                'acousticness': acousticness,
-                'instrumentalness': instrumentalness
-            }
-            items1.append(songs)
+    for song in songs_msgpack:
+        del song['mode'], song['speechiness'], song['acousticness'], song['instrumentalness']
 
-    return items1
+    return songs_msgpack
+
 
 def decode_csv(file_name2):
-    items2 = []
-    with open(file_name2, "r", encoding="utf-8") as input:
-        reader = csv.reader(input, delimiter=";")
-        next(reader)
-        for row in reader:
-            if len(row) == 0:
-                continue
-            item = dict()
-            item['artist'] = row[0]
-            item['song'] = row[1]
-            item['duration_ms'] = row[2]
-            item['year'] = row[3]
-            item['tempo'] = row[4]
-            item['genre'] = row[5]
-            item['energy'] = row[6]
-            item['key'] = row[7]
-            item['loudness'] = row[8]
+    with open(file_name2, "r", encoding="utf-8") as file:
+        songs_csv = csv.DictReader(file, delimiter=';') 
 
-            items2.append(item)
+        items2 = []
+        for song in songs_csv:
+            del song['energy'], song['key'], song['loudness']
+            items2.append(song)
+
     return items2
 
 file_name1 = "task_3_var_58_part_1.msgpack"
@@ -68,15 +34,14 @@ conn = sqlite3.connect('music.db')
 c = conn.cursor()
 db = sqlite3.connect('music.db')
 c.execute('''create table if not exists music 
-             (artist text, song text, duration_ms integer, year integer, tempo real, genre text, mode integer, speechiness real,
-             acousticness real, instrumentalness real)''')
+             (artist text, song text, duration_ms integer, year integer, tempo real, genre text)''')
 
 def insert_msgpack_data(db, data):
     cursor = db.cursor()
     cursor.executemany("""
-            insert into music (artist, song, duration_ms, year, tempo, genre, mode, speechiness, acousticness, instrumentalness)
+            insert into music (artist, song, duration_ms, year, tempo, genre)
             values (
-                :artist, :song, :duration_ms, :year, :tempo, :genre, :mode, :speechiness, :acousticness, :instrumentalness
+                :artist, :song, :duration_ms, :year, :tempo, :genre
             )""", data)
     db.commit()
 
@@ -110,7 +75,7 @@ def get_long_by_duration(db, limit):
 
 items = get_long_by_duration(conn, 68)
 with open('result_task_3.1.json', 'w') as f:
-    json.dump(items, f, indent=4)
+    json.dump(items, f, indent=4,ensure_ascii=False)
 
 # вывод (сумму, мин, макс, среднее) по произвольному числовому полю
 def get_stat(db):
@@ -131,7 +96,7 @@ def get_stat(db):
     cursor.close()
 
     with open('result_task_3.2.json', 'w') as f:
-        json.dump(result2_2, f, indent=4)
+        json.dump(result2_2, f, indent=4, ensure_ascii=False)
 
     return result2_2
 
@@ -141,7 +106,7 @@ result2 = get_stat(db)
 def freq(db):
     cursor = db.cursor()
     cursor.execute("""
-        SELECT mode, COUNT(*) as count
+        SELECT genre, COUNT(*) as count
         FROM music
     """)
 
@@ -152,7 +117,7 @@ def freq(db):
     cursor.close()
 
     with open('result_task_3.3.json', 'w') as f:
-        json.dump(result2_3, f, indent=4)
+        json.dump(result2_3, f, indent=4,ensure_ascii=False)
 
     return result2_3
 
@@ -176,7 +141,7 @@ def filter_by_year(db, min_year, limit=68):
     cursor.close()
 
     with open('result_task_3.4.json', 'w') as f:
-        json.dump(result, f, indent=4)
+        json.dump(result, f, indent=4, ensure_ascii=False)
 
     return result
 
